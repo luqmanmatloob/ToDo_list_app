1b906b06eda57e516bf96e740ad71e916198bcbc
luqmanmatloob <93182189+luqmanmatloob@users.noreply.github.com>
2024-02-06 18:03:50 +0500

    .env seting

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import Navbar from "@/components/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
+require('dotenv').config();
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="bg-[#b8feff] ">
       <body className={inter.className}>
-        <div className="max-w-3xl mx-auto p-4 bg-[#97f3f5] shadow-xl m-5 rounded-lg">
+        <div className="max-w-3xl mx-auto p-4 bg-[#a7dcdd] shadow-xl m-5 rounded-lg">
 
           <Navbar />
 
--- libs/mongodb.js
+++ libs/mongodb.js
@@ -4,10 +4,12 @@ import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
   try {
-    await mongoose.connect('mongodb://0.0.0.0:27017/todo');
+
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("Connected to MongoDB.");
   } catch (error) {
     console.log(error);
+
   }
 };
 
